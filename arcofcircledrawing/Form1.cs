920c3028f65e7aa071d0e268f2822bf37424b078
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-13 01:24:53 +0200

    Add project files.

---
+++ arcofcircledrawing/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arcofcircledrawing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private PointF point1;
+        private PointF point2;
+        private float radius;
+
+        public Form1(PointF point1, PointF point2, float radius)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.radius = radius;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            // Calculate the rectangle that bounds the circle
+            float x = Math.Min(point1.X, point2.X) - radius;
+            float y = Math.Min(point1.Y, point2.Y) - radius;
+            float width = Math.Abs(point1.X - point2.X) + 2 * radius;
+            float height = Math.Abs(point1.Y - point2.Y) + 2 * radius;
+            RectangleF rect = new RectangleF(x, y, width, height);
+
+            // Calculate the start and sweep angles
+            double startAngle = Math.Atan2(point1.Y - rect.Y - height / 2, point1.X - rect.X - width / 2) * 180 / Math.PI;
+            double endAngle = Math.Atan2(point2.Y - rect.Y - height / 2, point2.X - rect.X - width / 2) * 180 / Math.PI;
+            float sweepAngle = (float)(endAngle - startAngle);
+
+            // Draw the arc
+            using (Pen pen = new Pen(Color.Black))
+            {
+                g.DrawArc(pen, rect, (float)startAngle, sweepAngle);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ arcofcircledrawing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arcofcircledrawing
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Application.Run(new Form1(new PointF(50, 50), new PointF(100, 100), 30));
+        }
+    }
+}
